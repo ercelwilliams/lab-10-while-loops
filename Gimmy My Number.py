8017fe8d4267139a5b5739d47b81cbbe12b5a07b
Ercel  Williams <ercelwilliams@ercels-air.home>
2020-11-16 17:18:56 -0500

    labs

---
+++ Gimmy My Number.py
@@ -0,0 +1,12 @@
+userstring = prompt("Gimme a number that is greater than 100...")
+usernum = str(userstring, 10)
+
+while (usernum < 100)
+	print(usernum + " is less than 100, dummmy. Try again, I've got all day...")
+
+userstring = userinput(usernum + " is less than 100, dummmy. Try again, I've got all day...")
+usernum = str(userstring, 10)
+
+
+print("Congratulations! " + usernum + " is greater than 100! Great job!")
+
